60992f41bc750684dc0f2c37ab0d2f42909f2caa
TechDevAlex <alextheg3@gmail.com>
2023-11-21 18:49:03 +0100

    added description of some modules to the documentation

--- build/lib/src/data_conversion.py
+++ build/lib/src/data_conversion.py
@@ -1,8 +1,10 @@
 # src/data_conversion.py
 import pandas as pd
 
-def convert_to_dataframe(data):
-    # Assuming data is a list of tuples
-    columns = ["column1", "column2", "column3"]  # Replace with your actual column names
-    df = pd.DataFrame(data, columns=columns)
+def convert_to_dataframe(data, columns=None):
+    # If columns are not provided, infer them from the data
+    if columns is None:
+        df = pd.DataFrame(data)
+    else:
+        df = pd.DataFrame(data, columns=columns)
     return df
--- tests/test_data_conversion.py
+++ tests/test_data_conversion.py
@@ -4,7 +4,7 @@ from src.data_conversion import convert_to_dataframe
 
 class TestDataConversion(unittest.TestCase):
     def test_convert_to_dataframe(self):
-        data = [(1, 'A', 'X'), (2, 'B', 'Y'), (3, 'C', 'Z')]  # Update with your actual test data
+        data = [(1, 'A', 'X'), (2, 'B', 'Y'), (3, 'C', 'Z')]  # Update with actual test data
         df = convert_to_dataframe(data)
         self.assertIsNotNone(df)
-        # Add more assertions as needed
+        # more assertions
