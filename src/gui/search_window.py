f1d3b8fbe99a2bbad25becec280377cad5266934
Schlorchi00 <niko.nagenast@gmail.com>
2023-12-09 09:35:14 +1100

    Restructuring of Search Window

--- src/gui/search_window.py
+++ src/gui/search_window.py
@@ -6,53 +6,58 @@ from src.gui.controllers.search_logic_controller import SearchLogicController
 class SearchWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initializeSearchWindow()
 
-        self.setWindowTitle("Search Window")
-        self.setGeometry(100, 100, 400, 200)
 
-        # Widgets
-        search_label = QLabel("Search Query:", self)
-        self.query_input = QLineEdit(self)
 
-        table_label = QLabel("Table Name:", self)
-        self.table_input = QLineEdit(self)
+    def initializeSearchWindow(self):
 
-        self.search_button = QPushButton("Search", self)
+            self.setWindowTitle("Search Window")
+            self.setGeometry(100, 100, 400, 200)
 
-        self.search_options_label = QLabel("Select search options", self)
-        self.search_options_button = QComboBox(self)
-        self.search_options_button.addItems(['Name'])
+            # Widgets
+            search_label = QLabel("Search Query:", self)
+            self.query_input = QLineEdit(self)
 
-        self.results_table = QTableWidget(self)
-       
-        
+            table_label = QLabel("Table Name:", self)
+            self.table_input = QLineEdit(self)
 
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(search_label)
-        layout.addWidget(self.query_input)
-        layout.addWidget(table_label)
-        layout.addWidget(self.table_input)
-        layout.addWidget(self.search_button)
-        layout.addWidget(self.search_options_label)
-        layout.addWidget(self.search_options_button)
-        layout.addWidget(self.results_table)
-        
+            self.search_button = QPushButton("Search", self)
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+            self.search_options_label = QLabel("Select search options", self)
+            self.search_options_button = QComboBox(self)
+            self.search_options_button.addItems(['Name'])
 
-        # Keep a reference to the QFileDialog to prevent it from being garbage-collected
-        file_dialog = QFileDialog()
-        self.file_dialog = file_dialog
+            self.results_table = QTableWidget(self)
 
-        # Create an instance of SearchLogicController
-        self.controller = SearchLogicController(self)
+
+
+            # Layout
+            layout = QVBoxLayout()
+            layout.addWidget(search_label)
+            layout.addWidget(self.query_input)
+            layout.addWidget(table_label)
+            layout.addWidget(self.table_input)
+            layout.addWidget(self.search_button)
+            layout.addWidget(self.search_options_label)
+            layout.addWidget(self.search_options_button)
+            layout.addWidget(self.results_table)
+
+
+            container = QWidget()
+            container.setLayout(layout)
+            self.setCentralWidget(container)
+
+            # Keep a reference to the QFileDialog to prevent it from being garbage-collected
+            file_dialog = QFileDialog()
+            self.file_dialog = file_dialog
+
+            # Create an instance of SearchLogicController
+            self.controller = SearchLogicController(self)
+            self.show()
         
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     search_window = SearchWindow()
-    search_window.show()
     app.exec()
