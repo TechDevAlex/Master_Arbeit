7bf7b60714fe53383e4b74eb7b328f8ac4950ab9
Schlorchi00 <niko.nagenast@gmail.com>
2023-12-09 10:36:41 +1100

    Restructuring and TODOS

--- src/gui/search_window.py
+++ src/gui/search_window.py
@@ -19,18 +19,25 @@ class SearchWindow(QMainWindow):
             #search_label = QLabel("Search Query:", self)
             #self.query_input = QLineEdit(self)
 
-            table_label = QLabel("Table Name:", self)
+            table_table_label = QLabel("Names of uploaded tables:", self)
+            self.table_table = QTableWidget(self) #TODO: should list the uploaded table names
+
+            table_label = QLabel("Insert table name", self)
             self.table_input = QLineEdit(self)
 
-            self.search_button = QPushButton("Search", self)
-            self.search_button_2 = QPushButton("Search_2", self)
+            self.search_button = QPushButton("Display Table", self)
+            self.help_search = QPushButton("help", self) #TODO: Help menu should pop up
 
 
             self.search_options_label = QLabel("Select search options", self)
-            self.search_options_button = QComboBox(self)
-            self.search_options_button.addItems(['Name'])
+            self.search_options_button = QComboBox(self) #TODO: Click on option should pop up another window for further search refinement
+            self.search_options_button.addItems(['Simple Search', 'Parameter Search', 'Combined Search', 'Complete Search'])
+            self.help_search_options = QPushButton("help", self) #TODO: Help menu should pop up
 
-            self.results_table = QTableWidget(self)
+            self.results_table = QTableWidget(self) #TODO: Is it possible to click into the components of a table?
+
+            self.export_button = QPushButton('Export', self) #TODO: Export menu should pop up
+            self.clipboard_button = QPushButton('Clipboard',self) #TODO: Results should be stored at a clipboard
 
 
 
@@ -38,14 +45,18 @@ class SearchWindow(QMainWindow):
 
             layout = QGridLayout()
 
-
-            layout.addWidget(table_label,2,0)
-            layout.addWidget(self.table_input, 3,0)
-            layout.addWidget(self.search_button, 4,0)
-            layout.addWidget(self.search_button_2, 4,1)
-            layout.addWidget(self.search_options_label, 5,0)
-            layout.addWidget(self.search_options_button, 6,0)
-            layout.addWidget(self.results_table,7,0)
+            layout.addWidget(table_table_label, 1,0)
+            layout.addWidget(self.table_table,2,0)
+            layout.addWidget(table_label,3,0)
+            layout.addWidget(self.table_input, 4,0)
+            layout.addWidget(self.search_button, 5,0)
+            layout.addWidget(self.help_search, 5,1)
+            layout.addWidget(self.search_options_label, 6,0)
+            layout.addWidget(self.search_options_button, 7,0)
+            layout.addWidget(self.help_search_options, 7,1)
+            layout.addWidget(self.results_table,8,0)
+            layout.addWidget(self.export_button, 9,0)
+            layout.addWidget(self.clipboard_button, 9,1)
 
 
             container = QWidget()
