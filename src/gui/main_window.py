563bef25739ea56f2b186d52b3f1efb6f3bdc21d
Schlorchi00 <niko.nagenast@gmail.com>
2023-12-09 09:25:02 +1100

    Restructuring of the initialization

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -8,44 +8,49 @@ from src.gui.search_window import SearchWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initializeMainWindow()
 
-        # Create widgets from widgets.py
-        status_label = label("Status: ")
-        database_name_label = label("Database: Not Connected")
-        connect_button = small_button("Connect")
 
-        # Create the central widget
-        central_widget = QWidget()
+    def initializeMainWindow(self):
+            # Create widgets from widgets.py
+            status_label = label("Status: ")
+            database_name_label = label("Database: Not Connected")
+            connect_button = small_button("Connect")
 
-        # Set up the layout for the central widget
-        layout = QVBoxLayout(central_widget)
-        layout.addWidget(status_label)
-        layout.addWidget(database_name_label)
-        layout.addWidget(connect_button)
+            # Create the central widget
+            central_widget = QWidget()
 
-        # Add an "Initiate Data" button
-        self.data_button = data_button("Load Data")
-        self.data_button.setEnabled(False)  # Initially disabled
-        layout.addWidget(self.data_button)
-        
-        # Add a "Search" button
-        search_button = small_button("Search")
-        layout.addWidget(search_button)
+            # Set up the layout for the central widget
+            layout = QVBoxLayout(central_widget)
+            layout.addWidget(status_label)
+            layout.addWidget(database_name_label)
+            layout.addWidget(connect_button)
 
-        # Set the central widget for the main window
-        self.setCentralWidget(central_widget)
+            # Add an "Initiate Data" button
+            self.data_button = data_button("Load Data")
+            self.data_button.setEnabled(False)  # Initially disabled
+            layout.addWidget(self.data_button)
 
-        # Create the controller and pass the widgets to it
-        self.connection_controller = DBController(
-            status_label=status_label,
-            database_name_label=database_name_label,
-            connect_button=connect_button,
-            data_button=self.data_button
-        )
+            # Add a "Search" button
+            search_button = small_button("Search")
+            layout.addWidget(search_button)
 
-        # Connect the button click event to a function that opens the search window
-        search_button.clicked.connect(self.open_search_window)
+            # Set the central widget for the main window
+            self.setCentralWidget(central_widget)
 
+            # Create the controller and pass the widgets to it
+            self.connection_controller = DBController(
+                status_label=status_label,
+                database_name_label=database_name_label,
+                connect_button=connect_button,
+                data_button=self.data_button
+            )
+
+            # Connect the button click event to a function that opens the search window
+            search_button.clicked.connect(self.open_search_window)
+
+            self.setGeometry(100,100,250,150)
+            self.show()
 
     def open_search_window(self):
         # Create an instance of the search window
@@ -57,6 +62,6 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
-    window.setGeometry(100, 100, 400, 300)  # Set initial window size
-    window.show()
+    #window.setGeometry(100, 100, 400, 300)  # Set initial window size
+    #window.show()
     app.exec()
