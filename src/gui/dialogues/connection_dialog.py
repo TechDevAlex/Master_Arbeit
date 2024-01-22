d396ae0c88d988c197fcd97cbf5d739698b3f1d8
TechDevAlex <alextheg3@gmail.com>
2024-01-22 18:55:47 +0100

    dialogue directory

---
+++ src/gui/dialogues/connection_dialog.py
@@ -0,0 +1,27 @@
+from PyQt6.QtWidgets import QDialog, QLineEdit, QVBoxLayout, QPushButton, QLabel
+
+class ConnectionDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Connect to Database")
+
+        layout = QVBoxLayout()
+
+        self.username_input = QLineEdit()
+        self.username_input.setPlaceholderText("Username")
+        layout.addWidget(self.username_input)
+
+        self.password_input = QLineEdit()
+        self.password_input.setPlaceholderText("Password")
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.password_input)
+
+        self.license_input = QLineEdit()
+        self.license_input.setPlaceholderText("License Code (optional)")
+        layout.addWidget(self.license_input)
+
+        connect_button = QPushButton("Connect")
+        connect_button.clicked.connect(self.accept)
+        layout.addWidget(connect_button)
+
+        self.setLayout(layout)
